064b21c472e0fe1a1d9e7ac4b7c7fc80dd23ffc4
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2010-02-15 09:33:21 +0000

    git-svn-id: https://chibios.svn.sourceforge.net/svnroot/chibios/branches/ppc_dev@1613 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/hal/platforms/SPC563/platform.mk
@@ -0,0 +1,5 @@
+# List of all the SPC563 platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/SPC563/hal_lld.c
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/SPC563
