1967237aa62b037aed7e7e8cdc7c80c0123f776f
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2010-02-23 14:00:27 +0000

    git-svn-id: https://chibios.svn.sourceforge.net/svnroot/chibios/branches/stm8_dev@1670 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ demos/STM8S-STM8AF51-RC/main.c
@@ -0,0 +1,56 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ch.h"
+#include "hal.h"
+/*#include "test.h"*/
+
+/*
+ * Entry point.
+ */
+void main(void) {
+
+  /*
+   * Board/HAL initialization.
+   */
+  hwinit();
+
+  /*
+   * Activates the serial driver 1 and 3 using the driver default
+   * configuration.
+   */
+  sdStart(&SD1, NULL);
+  sdStart(&SD3, NULL);
+
+  /*
+   * OS initialization.
+   */
+  chSysInit();
+
+  /*
+   * Normal main() thread activity.
+   */
+  while (!chThdShouldTerminate()) {
+/*    volatile msg_t result;
+    result = TestThread(&SD1);*/
+    sdWriteTimeout(&SD1, "Hello World!\r\n", 14, TIME_INFINITE);
+    sdWriteTimeout(&SD3, "Hello World!\r\n", 14, TIME_INFINITE);
+    chThdSleepMilliseconds(10000);
+  }
+}
