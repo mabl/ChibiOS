7483b643807b7845f0ea93c6e100f9174199c2e4
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-07-31 08:42:51 +0000

    git-svn-id: https://chibios.svn.sourceforge.net/svnroot/chibios/branches/utils_dev@4490 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ config_wizard/src/org/chibios/tools/eclipse/config/handlers/CheckDescription.java
@@ -0,0 +1,34 @@
+package org.chibios.tools.eclipse.config.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class CheckDescription extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public CheckDescription() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"ChibiOS-RT_Configuration_Support",
+				"Hello, Eclipse world");
+		return null;
+	}
+}
