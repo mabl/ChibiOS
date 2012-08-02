556ae5e5455c557b186151c9add9ec1a39a0fab1
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-08-02 18:02:14 +0000

    git-svn-id: https://chibios.svn.sourceforge.net/svnroot/chibios/branches/utils_dev@4496 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- config_wizard/src/org/chibios/tools/eclipse/config/handlers/GenerateFiles.java
+++ config_wizard/src/org/chibios/tools/eclipse/config/handlers/GenerateFiles.java
@@ -16,39 +16,59 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package org.chibios.tools.eclipse.config.handlers;
+
+import java.io.File;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class GenerateFiles extends AbstractHandler {
-	/**
-	 * The constructor.
-	 */
-	public GenerateFiles() {
-	}
 
-	/**
-	 * the command has been executed, so extract extract the needed information
-	 * from the application context.
-	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"ChibiOS-RT_Configuration_Support",
-				"Hello, Eclipse world");
-		return null;
-	}
+  /**
+   * The constructor.
+   */
+  public GenerateFiles() {
+  }
+
+  /**
+   * the command has been executed, so extract extract the needed information
+   * from the application context.
+   */
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    IWorkbenchWindow window = HandlerUtil
+        .getActiveWorkbenchWindowChecked(event);
+    ISelection selection = window.getSelectionService().getSelection();
+    if (selection instanceof IStructuredSelection) {
+      IStructuredSelection sselection = (IStructuredSelection)selection;
+      IFile file = (IFile)sselection.getFirstElement();
+
+      IPath rootpath = ResourcesPlugin.getWorkspace().getRoot().getFullPath();
+      IPath filepath = rootpath.addTrailingSeparator().append(file.getFullPath());
+
+      MessageDialog.openInformation(window.getShell(),
+          "ChibiOS-RT:", filepath.toString());
+    }
+    return null;
+  }
 }
