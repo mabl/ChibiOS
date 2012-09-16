40b75023350ae71325647a619bb8dafb4e59449c
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-09-16 18:03:42 +0000

    Fixed bug 3566342.
    git-svn-id: https://chibios.svn.sourceforge.net/svnroot/chibios/branches/stable_2.2.x@4675 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/ports/cosmic/STM8/chcore.h
+++ os/ports/cosmic/STM8/chcore.h
@@ -87,6 +87,7 @@ typedef void (*stm8func_t)(void);
  */
 struct extctx {
   uint8_t       _next;
+  uint8_t       c_lreg[4];
   uint8_t       c_y[3];
   uint8_t       c_x[3];
   uint8_t       cc;
@@ -209,7 +210,7 @@ struct stm8_startctx {
  * @note    @p id can be a function name or a vector number depending on the
  *          port implementation.
  */
-#define PORT_IRQ_HANDLER(id) @far @interrupt void vector##id(void)
+#define PORT_IRQ_HANDLER(id) @far @interrupt @svlreg void vector##id(void)
 
 /**
  * @brief   Port-related initialization code.
