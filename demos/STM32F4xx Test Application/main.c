f17e8a99ee01b2553513bf95879a04643640ec19
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-02-18 19:50:56 +0000

    git-svn-id: https://chibios.svn.sourceforge.net/svnroot/chibios/branches/spc5studio_components_trunk@5254 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/STM32F4xx Test Application/main.c
+++ demos/STM32F4xx Test Application/main.c
@@ -24,6 +24,24 @@
 #include "components.h"
 
 /*
+ * This is a periodic thread that does absolutely nothing except flashing
+ * a LED.
+ */
+static WORKING_AREA(waThread1, 128);
+static msg_t Thread1(void *arg) {
+
+  (void)arg;
+  chRegSetThreadName("blinker");
+  while (TRUE) {
+    palSetPad(GPIOD, GPIOD_LED_ORANGE);
+    chThdSleepMilliseconds(500);
+    palClearPad(GPIOD, GPIOD_LED_ORANGE);
+    chThdSleepMilliseconds(500);
+  }
+  return 0;
+}
+
+/*
  * Application entry point.
  */
 int main(void) {
@@ -32,8 +50,28 @@ int main(void) {
      the application wizard. The function is generated automatically.*/
   componentsInit();
 
-  /* Application main loop.*/
-  while (1) {
+  /*
+   * Activates the serial driver 2 using the driver default configuration.
+   * PA2(TX) and PA3(RX) are routed to USART2.
+   */
+  sdStart(&SD2, NULL);
+  palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
+  palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
 
+  /*
+   * Creates the blinker thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  /*
+   * Normal main() thread activity, in this demo it does nothing except
+   * sleeping in a loop and check the button state, when the button is
+   * pressed the test procedure is launched with output on the serial
+   * driver 2.
+   */
+  while (TRUE) {
+    if (palReadPad(GPIOA, GPIOA_BUTTON))
+      TestThread(&SD2);
+    chThdSleepMilliseconds(500);
   }
 }
