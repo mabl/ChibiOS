f2ea90f2ce4df132575f6769898002300369aa57
Dave Camarillo <dave.camarillo@gmail.com>
2013-02-21 23:06:54 -0800

    removed old device descriptor cruft

--- demos/ARMCM4-STM32F407-WAVESHARE-OPEN-407I-C-USB_HS_ULPI-USB-MASS-STORAGE-VCOM/usb_msd_cfg.c
+++ demos/ARMCM4-STM32F407-WAVESHARE-OPEN-407I-C-USB_HS_ULPI-USB-MASS-STORAGE-VCOM/usb_msd_cfg.c
@@ -62,125 +62,6 @@
 #endif
 
 
-
-#if FALSE
-/*
- * USB Device Descriptor.
- */
-static const uint8_t msd_device_descriptor_data[18] = {
-  USB_DESC_DEVICE       (0x0200,        /* bcdUSB (2.0).                    */
-                         0x00,          /* bDeviceClass (None).             */
-                         0x00,          /* bDeviceSubClass.                 */
-                         0x00,          /* bDeviceProtocol.                 */
-                         0x40,          /* Control Endpoint Size.           */
-                         0x0483,        /* idVendor (ST).                   */
-                         0x5742,        /* idProduct.                       */
-                         0x0100,        /* bcdDevice.                       */
-                         1,             /* iManufacturer.                   */
-                         2,             /* iProduct.                        */
-                         3,             /* iSerialNumber.                   */
-                         1)             /* bNumConfigurations.              */
-};
-
-/*
- * Device Descriptor wrapper.
- */
-static const USBDescriptor msd_device_descriptor = {
-  sizeof msd_device_descriptor_data,
-  msd_device_descriptor_data
-};
-
-/* Configuration Descriptor tree for a CDC.*/
-static const uint8_t msd_configuration_descriptor_data[] = {
-  /* Configuration Descriptor.*/
-  USB_DESC_CONFIGURATION(0x0020,        /* wTotalLength.                    */
-                         0x01,          /* bNumInterfaces.                  */
-                         0x01,          /* bConfigurationValue.             */
-                         0,             /* iConfiguration.                  */
-                         0xC0,          /* bmAttributes (self powered).     */
-                         0x32),           /* bMaxPower (100mA).               */
-  /* Interface Descriptor.*/
-  USB_DESC_INTERFACE    (0x00,          /* bInterfaceNumber.                */
-                         0x00,          /* bAlternateSetting.               */
-                         0x02,          /* bNumEndpoints.                   */
-                         0x08,          /* bInterfaceClass (Mass Storage)   */
-                         0x06,          /* bInterfaceSubClass (SCSI
-                                           Transparent storage class)       */
-                         0x50,          /* bInterfaceProtocol (Bulk Only)   */
-                         0),            /* iInterface. (none)               */
-  /* Mass Storage Data In Endpoint Descriptor.*/
-  USB_DESC_ENDPOINT     (USB_MS_DATA_EP|0x80,
-                         0x02,          /* bmAttributes (Bulk).             */
-                         USB_MAX_PACKET_SIZE,/* wMaxPacketSize.                  */
-                         0x05),         /* bInterval. 1ms                   */
-  /* Mass Storage Data In Endpoint Descriptor.*/
-  USB_DESC_ENDPOINT     (USB_MS_DATA_EP,
-                         0x02,          /* bmAttributes (Bulk).             */
-                         USB_MAX_PACKET_SIZE,/* wMaxPacketSize.                  */
-                         0x05)          /* bInterval. 1ms                   */
-};
-
-/*
- * Configuration Descriptor wrapper.
- */
-static const USBDescriptor msd_configuration_descriptor = {
-  sizeof msd_configuration_descriptor_data,
-  msd_configuration_descriptor_data
-};
-
-/*
- * U.S. English language identifier.
- */
-static const uint8_t msd_string0[] = {
-  USB_DESC_BYTE(4),                     /* bLength.                         */
-  USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-  USB_DESC_WORD(0x0409)                 /* wLANGID (U.S. English).          */
-};
-
-/*
- * Vendor string.
- */
-static const uint8_t msd_string1[] = {
-  USB_DESC_BYTE(38),                    /* bLength.                         */
-  USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-  'S', 0, 'T', 0, 'M', 0, 'i', 0, 'c', 0, 'r', 0, 'o', 0, 'e', 0,
-  'l', 0, 'e', 0, 'c', 0, 't', 0, 'r', 0, 'o', 0, 'n', 0, 'i', 0,
-  'c', 0, 's', 0
-};
-
-/*
- * Device Description string.
- */
-static const uint8_t msd_string2[] = {
-  USB_DESC_BYTE(62),                    /* bLength.                         */
-  USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-  'C', 0, 'h', 0, 'i', 0, 'b', 0, 'i', 0, 'O', 0, 'S', 0, '/', 0,
-  'R', 0, 'T', 0, ' ', 0, 'M', 0, 'a', 0, 's', 0, 's', 0, ' ', 0,
-  'S', 0, 't', 0, 'o', 0, 'r', 0, 'a', 0, 'g', 0, 'e', 0, ' ', 0,
-  'D', 0, 'e', 0, 'v', 0, 'i', 0, 'c', 0, 'e'
-};
-
-static const uint8_t msd_string3[] = {
-  USB_DESC_BYTE(26),                     /* bLength.                         */
-  USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-  'A', 0, 'E', 0, 'C', 0, 'C', 0, 'E', 0, 'C', 0, 'C', 0, 'C', 0, 'C', 0,
-  '0' + CH_KERNEL_MAJOR, 0,
-  '0' + CH_KERNEL_MINOR, 0,
-  '0' + CH_KERNEL_PATCH, 0
-};
-
-/*
- * Strings wrappers array.
- */
-static const USBDescriptor msd_strings[] = {
-  {sizeof msd_string0, msd_string0},
-  {sizeof msd_string1, msd_string1},
-  {sizeof msd_string2, msd_string2},
-  {sizeof msd_string3, msd_string3}
-};
-
-#else
-
 /*
  * USB Device Descriptor.
  */
@@ -370,10 +251,6 @@ const USBDescriptor msd_strings[] = {
   {sizeof msd_string2, msd_string2},
   {sizeof msd_string3, msd_string3},
 };
-
-
-
-#endif
 
 
 
