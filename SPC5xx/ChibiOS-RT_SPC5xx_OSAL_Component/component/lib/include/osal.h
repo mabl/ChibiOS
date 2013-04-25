f14a6528100f75a7cd431667944f18e43bab1081
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-04-25 16:09:48 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/spc5studio_components_trunk@5609 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ SPC5xx/ChibiOS-RT_SPC5xx_OSAL_Component/component/lib/include/osal.h
@@ -0,0 +1,628 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
+                 2011,2012,2013 Giovanni Di Sirio.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    osal.h
+ * @brief   OSAL module header.
+ *
+ * @addtogroup OSAL
+ * @{
+ */
+
+#ifndef _OSAL_H_
+#define _OSAL_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "ch.h"
+
+/*===========================================================================*/
+/* Module constants.                                                         */
+/*===========================================================================*/
+
+/**
+ * @name    Common constants
+ * @{
+ */
+#if !defined(FALSE) || defined(__DOXYGEN__)
+#define FALSE                               false
+#endif
+
+#if !defined(TRUE) || defined(__DOXYGEN__)
+#define TRUE                                true
+#endif
+
+#define OSAL_SUCCESS                        FALSE
+#define OSAL_FAILED                         TRUE
+/** @} */
+
+/**
+ * @name    Messages
+ * @{
+ */
+#define MSG_OK              RDY_OK
+#define MSG_RESET           RDY_RESET
+#define MSG_TIMEOUT         RDY_TIMEOUT
+/** @} */
+
+/*===========================================================================*/
+/* Module pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module data structures and types.                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   Type of a Virtual Timer structure.
+ * @note 	Mapped on ChibiOS/RT's own virtual timers.
+ */
+typedef VirtualTimer virtual_timer_t;
+
+/**
+ * @brief   Type of a thread.
+ * @note 	Mapped on ChibiOS/RT's own threads.
+ */
+typedef Thread thread_t;
+
+/**
+ * @brief   Type of a thread reference.
+ */
+typedef thread_t * thread_reference_t;
+
+/**
+ * @brief   Type of an event flags mask.
+ * @note 	Mapped on ChibiOS/RT's own flags.
+ */
+typedef flagsmask_t eventflags_t;
+
+/**
+ * @brief   Type of an event flags object.
+ * @note 	Mapped on ChibiOS/RT's own event source object.
+ */
+typedef EventSource eventsource_t;
+
+/**
+ * @brief   Type of a mutex.
+ * @note 	Mapped on ChibiOS/RT's own event mutexes or semaphores.
+ */
+#if CH_USE_MUTEXES || defined(__DOXYGEN__)
+typedef Mutex mutex_t;
+#elif CH_USE_SEMAPHORES
+typedef Semaphore mutex_t;
+#else
+typedef uint32_t mutex_t;
+#endif
+
+/*===========================================================================*/
+/* Module macros and inline functions.                                       */
+/*===========================================================================*/
+
+/**
+ * @brief   OSAL module initialization.
+ *
+ * @api
+ */
+static inline
+void osalInit(void) {
+
+}
+
+/**
+ * @brief   Condition assertion.
+ * @note 	Mapped on ChibiOS/RT's own assertions.
+ *
+ * @param[in] c         the condition to be verified to be true
+ * @param[in] msg       the text message
+ * @param[in] remark    a remark string
+ *
+ * @api
+ */
+#define osalDbgAssert(c, msg, remark) chDbgAssert(c, msg, remark)
+/**
+ * @brief   Function parameters check.
+ * @note 	Mapped on ChibiOS/RT's own checks.
+ *
+ * @param[in] c         the condition to be verified to be true
+ *
+ * @api
+ */
+#define osalDbgCheck(c) chDbgCheck(c, __FUNCTION__)
+
+/**
+ * @brief   I-Class state check.
+ * @note 	Mapped on ChibiOS/RT's own state checker.
+ */
+#define osalDbgCheckClassI() chDbgCheckClassI()
+
+/**
+ * @brief   S-Class state check.
+ * @note 	Mapped on ChibiOS/RT's own state checker.
+ */
+#define osalDbgCheckClassS() chDbgCheckClassS()
+
+/**
+ * @brief   IRQ prologue code.
+ * @note 	Mapped on ChibiOS/RT's own ISR macros.
+ */
+#define OSAL_IRQ_PROLOGUE() CH_IRQ_PROLOGUE()
+
+/**
+ * @brief   IRQ epilogue code.
+ * @note 	Mapped on ChibiOS/RT's own ISR macros.
+ */
+#define OSAL_IRQ_EPILOGUE() CH_IRQ_EPILOGUE()
+
+/**
+ * @brief   IRQ handler function declaration.
+ * @note 	Mapped on ChibiOS/RT's own ISR macros.
+ *
+ * @param[in] id        a vector name as defined in @p vectors.s
+ */
+#define OSAL_IRQ_HANDLER(id) CH_IRQ_HANDLER(id)
+
+/**
+ * @brief   Globally enables interrupts.
+ * @note 	Mapped on ChibiOS/RT's own ISR primitives.
+ *
+ * @special
+ */
+static inline
+void osalIsrEnable(void) {
+
+	chSysEnable();
+}
+
+/**
+ * @brief   Globally disables interrupts.
+ * @note 	Mapped on ChibiOS/RT's own ISR primitives.
+ *
+ * @special
+ */
+static inline
+void osalIsrDisable(void) {
+
+	chSysDisable();
+}
+
+/**
+ * @brief   Waits for an interrupt to occur.
+ * @note 	Mapped on ChibiOS/RT's own ISR primitives.
+ *
+ * @special
+ */
+static inline
+void osalIsrWait(void) {
+
+  chSysLockFromIsr();
+}
+
+/**
+ * @brief   System halt with error message.
+ *
+ * @param[in] reason    the halt message pointer
+ *
+ * @api
+ */
+static inline
+void osalSysHalt(const char *reason) {
+
+  chDbgAssert(0, reason, "");
+  chSysHalt();
+}
+
+/**
+ * @brief   System timer handler.
+ * @details The handler is used for scheduling and Virtual Timers management.
+ *
+ * @iclass
+ */
+static inline
+void osalSysTimerHandlerI(void) {
+
+  chSysTimerHandlerI();
+}
+
+/**
+ * @brief   Enters a critical zone from thread context.
+ * @note    This function cannot be used for reentrant critical zones.
+ * @note 	Mapped on ChibiOS/RT's own lock primitives.
+ *
+ * @special
+ */
+static inline
+void osalSysLock(void) {
+
+  chSysLock();
+}
+
+/**
+ * @brief   Leaves a critical zone from thread context.
+ * @note    This function cannot be used for reentrant critical zones.
+ * @note 	Mapped on ChibiOS/RT's own lock primitives.
+ *
+ * @special
+ */
+static inline
+void osalSysUnlock(void) {
+
+  chSysUnlock();
+}
+
+/**
+ * @brief   Enters a critical zone from ISR context.
+ * @note    This function cannot be used for reentrant critical zones.
+ * @note 	Mapped on ChibiOS/RT's own lock primitives.
+ *
+ * @special
+ */
+static inline
+void osalSysLockFromISR(void) {
+
+  chSysLockFromIsr();
+}
+
+/**
+ * @brief   Leaves a critical zone from ISR context.
+ * @note    This function cannot be used for reentrant critical zones.
+ * @note 	Mapped on ChibiOS/RT's own lock primitives.
+ *
+ * @special
+ */
+static inline
+void osalSysUnlockFromISR(void) {
+
+  chSysUnlockFromIsr();
+}
+
+/**
+ * @brief   Returns @p TRUE if the specified timer is armed.
+ * @note 	Mapped on ChibiOS/RT's own virtual timers.
+ *
+ * @param[out] vtp      the @p virtual_timer_t structure pointer
+ *
+ * @iclass
+ */
+static inline
+bool osalVTIsArmedI(virtual_timer_t *vtp) {
+
+  return (bool)chVTIsArmedI(vtp);
+}
+
+/**
+ * @brief   Enables a virtual timer.
+ * @note    The associated function is invoked from interrupt context.
+ * @note    Mapped on ChibiOS/RT's own virtual timers.
+ *
+ * @param[out] vtp      the @p virtual_timer_t structure pointer
+ * @param[in] time      the number of ticks before the operation timeouts, the
+ *                      special values are handled as follow:
+ *                      - @a TIME_INFINITE is allowed but interpreted as a
+ *                        normal time specification.
+ *                      - @a TIME_IMMEDIATE this value is not allowed.
+ *                      .
+ * @param[in] vtfunc    the timer callback function. After invoking the
+ *                      callback the timer is disabled and the structure can
+ *                      be disposed or reused.
+ * @param[in] par       a parameter that will be passed to the callback
+ *                      function
+ *
+ * @iclass
+ */
+static inline
+void osalVTSetI(virtual_timer_t *vtp, systime_t time,
+                vtfunc_t vtfunc, void *par) {
+
+  chVTSetI((VirtualTimer *)vtp, time, vtfunc, par);
+}
+
+/**
+ * @brief   Enables a virtual timer.
+ * @note    The associated function is invoked from interrupt context.
+ * @note    Mapped on ChibiOS/RT's own virtual timers.
+ *
+ * @param[out] vtp      the @p virtual_timer_t structure pointer
+ * @param[in] time      the number of ticks before the operation timeouts, the
+ *                      special values are handled as follow:
+ *                      - @a TIME_INFINITE is allowed but interpreted as a
+ *                        normal time specification.
+ *                      - @a TIME_IMMEDIATE this value is not allowed.
+ *                      .
+ * @param[in] vtfunc    the timer callback function. After invoking the
+ *                      callback the timer is disabled and the structure can
+ *                      be disposed or reused.
+ * @param[in] par       a parameter that will be passed to the callback
+ *                      function
+ *
+ * @api
+ */
+static inline
+void osalVTSet(virtual_timer_t *vtp, systime_t time,
+               vtfunc_t vtfunc, void *par) {
+
+  chVTSet((VirtualTimer *)vtp, time, vtfunc, par);
+}
+
+/**
+ * @brief   Disables a Virtual Timer.
+ * @note    The timer MUST be active when this function is invoked.
+ * @note    Mapped on ChibiOS/RT's own virtual timers.
+ *
+ * @param[in] vtp       the @p virtual_timer_t structure pointer
+ *
+ * @iclass
+ */
+static inline
+void osalVTResetI(virtual_timer_t *vtp) {
+
+  chVTResetI((VirtualTimer *)vtp);
+}
+
+/**
+ * @brief   Disables a Virtual Timer.
+ * @note    The timer is first checked and disabled only if armed.
+ * @note    Mapped on ChibiOS/RT's own virtual timers.
+ *
+ * @param[in] vtp       the @p virtual_timer_t structure pointer
+ *
+ * @api
+ */
+static inline
+void chVTReset(virtual_timer_t *vtp) {
+
+  chVTReset((VirtualTimer *)vtp);
+}
+
+/**
+ * @brief   Checks if the current system time is within the specified time
+ *          window.
+ * @note    When start==end then the function returns always true because the
+ *          whole time range is specified.
+ * @note    Mapped on ChibiOS/RT's own virtual timers.
+ *
+ * @param[in] start     the start of the time window (inclusive)
+ * @param[in] end       the end of the time window (non inclusive)
+ * @retval TRUE         current time within the specified time window.
+ * @retval FALSE        current time not within the specified time window.
+ *
+ * @api
+ */
+static inline
+bool osalVTIsSystemTimeWithin(systime_t start, systime_t end) {
+
+  return (bool)chTimeIsWithin(start, end);
+}
+
+/**
+ * @brief   Current system time.
+ * @details Returns the number of system ticks since the @p osalInit()
+ *          invocation.
+ * @note    The counter can reach its maximum and then restart from zero.
+ * @note 	Mapped on ChibiOS/RT's own virtual timers.
+ *
+ * @return              The system time in ticks.
+ *
+ * @api
+ */
+static inline
+systime_t osalVTGetSystemTime(void) {
+
+  return chTimeNow();
+}
+
+/**
+ * @brief   Suspends the invoking thread for the specified time.
+ *
+ * @param[in] time      the delay in system ticks, the special values are
+ *                      handled as follow:
+ *                      - @a TIME_INFINITE the thread enters an infinite sleep
+ *                        state.
+ *                      - @a TIME_IMMEDIATE this value is not allowed.
+ *                      .
+ *
+ * @api
+ */
+static inline
+void osalThreadSleep(systime_t time) {
+
+  chThdSleepSeconds(time)
+}
+
+/**
+ * @brief   Delays the invoking thread for the specified number of seconds.
+ * @note    The specified time is rounded up to a value allowed by the real
+ *          system tick clock.
+ * @note    The maximum specifiable value is implementation dependent.
+ *
+ * @param[in] sec       time in seconds, must be different from zero
+ *
+ * @api
+ */
+#define osalThreadSleepSeconds(sec) chThdSleepSeconds(sec)
+
+/**
+ * @brief   Delays the invoking thread for the specified number of
+ *          milliseconds.
+ * @note    The specified time is rounded up to a value allowed by the real
+ *          system tick clock.
+ * @note    The maximum specifiable value is implementation dependent.
+ *
+ * @param[in] msec      time in milliseconds, must be different from zero
+ *
+ * @api
+ */
+#define osalThreadSleepMilliseconds(msec) chThdSleepMilliseconds(msec)
+
+/**
+ * @brief   Delays the invoking thread for the specified number of
+ *          microseconds.
+ * @note    The specified time is rounded up to a value allowed by the real
+ *          system tick clock.
+ * @note    The maximum specifiable value is implementation dependent.
+ *
+ * @param[in] usec      time in microseconds, must be different from zero
+ *
+ * @api
+ */
+#define osalThreadSleepMicroseconds(usec) chThdSleepMicroseconds(usec)
+
+/**
+ * @brief   Checks if a reschedule is required and performs it.
+ * @note    I-Class functions invoked from thread context must not reschedule
+ *          by themselves, an explicit reschedule using this function is
+ *          required in this scenario.
+ *
+ * @sclass
+ */
+static inline
+void osalOsRescheduleS(void) {
+
+  chThdRescheduleS();
+}
+
+/**
+ * @brief   Initializes an event flags object.
+ *
+ * @param[in] esp       pointer to the event flags object
+ *
+ * @init
+ */
+static inline
+void osalEventInit(eventsource_t *esp) {
+
+  chEvtInit((EventSource *)esp);
+}
+
+/**
+ * @brief   Add flags to an event source object.
+ *
+ * @param[in] esp       pointer to the event flags object
+ * @param[in] flags     flags to be ORed to the flags mask
+ *
+ * @iclass
+ */
+static inline
+void osalEventBroadcastI(eventsource_t *esp,
+                         eventflags_t flags) {
+
+  chEvtBroadcastI((EventSource *)esp, (flagsmask_t)flags);
+}
+
+/**
+ * @brief   Add flags to an event source object.
+ *
+ * @param[in] esp       pointer to the event flags object
+ * @param[in] flags     flags to be ORed to the flags mask
+ *
+ * @api
+ */
+static inline
+void osalEventBroadcast(eventsource_t *esp,
+                        eventflags_t flags) {
+
+  chEvtBroadcast((EventSource *)esp, (flagsmask_t)flags);
+}
+
+/**
+ * @brief   Initializes s @p mutex_t object.
+ *
+ * @param[out] mp       pointer to the @p mutex_t object
+ *
+ * @init
+ */
+static inline
+void osalMutexInit(mutex_t *mp) {
+
+#if CH_USE_MUTEXES
+  chMtxInit((Mutex *)mp);
+#elif CH_USE_SEMAPHORES
+  chSemInit((Semaphore *)mp, 1);
+#else
+ *mp = 0;
+#endif
+}
+
+/*
+ * @brief   Locks the specified mutex.
+ * @post    The mutex is locked and inserted in the per-thread stack of owned
+ *          mutexes.
+ *
+ * @param[in,out] mp    pointer to the @p mutex_t object
+ *
+ * @api
+ */
+static inline
+void osalMutexLock(mutex_t *mp) {
+
+#if CH_USE_MUTEXES
+  chMtxLock((Mutex *)mp);
+#elif CH_USE_SEMAPHORES
+  chSemWait((Semaphore *)mp);
+#else
+  *mp = 1;
+#endif
+}
+
+/**
+ * @brief   Unlocks the specified mutex.
+ * @note    The HAL guarantees to release mutex in reverse lock order. The
+ *          mutex being unlocked is guaranteed to be the last locked mutex
+ *          by the invoking thread.
+ *          The implementation can rely on this behavior and eventually
+ *          ignore the @p mp parameter which is supplied in order to support
+ *          those OSes not supporting a stack of the owned mutexes.
+ *
+ * @param[in,out] mp    pointer to the @p mutex_t object
+ *
+ * @api
+ */
+static inline
+void osalMutexUnlock(mutex_t *mp) {
+
+#if CH_USE_MUTEXES
+  (void)mp;
+  chMtxUnlock();
+#elif CH_USE_SEMAPHORES
+  chSemSignal((Semaphore *)mp);
+#else
+  *mp = 0;
+#endif
+}
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  msg_t osalThreadSuspendS(thread_reference_t *trp);
+  void osalThreadResumeI(thread_reference_t *trp, msg_t msg);
+  void osalThreadResumeS(thread_reference_t *trp, msg_t msg);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _OSAL_H_ */
+
+/** @} */
---
+++ SPC5xx/ChibiOS-RT_SPC5xx_OSAL_Component/component/lib/src/osal.c
@@ -0,0 +1,130 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
+                 2011,2012,2013 Giovanni Di Sirio.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    osal.c
+ * @brief   OSAL module code.
+ *
+ * @addtogroup OSAL
+ * @{
+ */
+
+#include "osal.h"
+
+/*===========================================================================*/
+/* Module local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module exported variables.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   Pointer to a halt error message.
+ * @note    The message is meant to be retrieved by the debugger after the
+ *          system halt caused by an unexpected error.
+ */
+const char *osal_halt_msg;
+
+/**
+ * @brief   Virtual timers delta list header.
+ */
+virtual_timers_list_t vtlist;
+
+/*===========================================================================*/
+/* Module local types.                                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local variables.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module exported functions.                                                */
+/*===========================================================================*/
+
+
+/**
+ * @brief   Sends the current thread sleeping and sets a reference variable.
+ * @note    This function must reschedule, it can only be called from thread
+ *          context.
+ *
+ * @param[in] trp       a pointer to a thread reference object
+ * @return              The wake up message.
+ *
+ * @sclass
+ */
+msg_t osalThreadSuspendS(thread_reference_t *trp) {
+
+  chDbgAssert(*trp == NULL, "osalThreadSuspendS(), #1", "not NULL");
+
+  *trp = (thread_reference_t)chThdSelf();
+  chSchGoSleepS(THD_STATE_SUSPENDED);
+  return self.message;
+}
+
+/**
+ * @brief   Wakes up a thread waiting on a thread reference object.
+ * @note    This function must not reschedule because it can be called from
+ *          ISR context.
+ *
+ * @param[in] trp       a pointer to a thread reference object
+ * @param[in] msg       the message code
+ *
+ * @iclass
+ */
+void osalThreadResumeI(thread_reference_t *trp, msg_t msg) {
+
+  if (*trp != NULL) {
+
+    chDbgAssert((*trp)->p_state == THD_STATE_SUSPENDED,
+                "osalThreadResumeI(), #1", "not THD_STATE_SUSPENDED");
+
+    (*trp)->p_u.rdymsg = msg;
+    chSchReadyI((Thread *)*trp);
+    *trp = NULL;
+  }
+}
+
+/**
+ * @brief   Wakes up a thread waiting on a thread reference object.
+ * @note    This function must reschedule, it can only be called from thread
+ *          context.
+ *
+ * @param[in] trp       a pointer to a thread reference object
+ * @param[in] msg       the message code
+ *
+ * @iclass
+ */
+void osalThreadResumeS(thread_reference_t *trp, msg_t msg) {
+
+  if (*trp != NULL) {
+    Thread *tp = (Thread *)*trp;
+
+    chDbgAssert(tp->p_state == THD_STATE_SUSPENDED,
+                "osalThreadResumeS(), #1", "not THD_STATE_SUSPENDED");
+
+    *trp = NULL;
+    chSchWakeupS(tp, msg);
+  }
+}
+
+/** @} */
