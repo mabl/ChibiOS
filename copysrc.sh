1f45bb8441ec5a99abcf7e70d61960e158e31ad8
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-05-05 12:41:12 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/spc5studio_components_trunk@5684 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- copysrc.sh
+++ copysrc.sh
@@ -9,6 +9,7 @@ chibios=/cygdrive/d/Progetti/ChibiOS-RT-stable-2.6.x
 #============================================================================#
 
 # Portable Kernel.
+echo "Portable Kernel"
 if [ ! -d $do_templates ]
 then
   cp ${chibios}/os/kernel/templates/chconf.h ./portable/ChibiOS-RT_Portable_Kernel_Component/component/cfg/chconf.h.ftl
@@ -17,6 +18,7 @@ cp -R ${chibios}/os/kernel/include ./portable/ChibiOS-RT_Portable_Kernel_Compone
 cp -R ${chibios}/os/kernel/src ./portable/ChibiOS-RT_Portable_Kernel_Component/component/lib
 
 # Portable HAL.
+echo "Portable HAL"
 if [ ! -d $do_templates ]
 then
   cp ${chibios}/os/hal/templates/halconf.h ./portable/ChibiOS-RT_Portable_HAL_Component/component/cfg/halconf.h.ftl
@@ -25,14 +27,17 @@ cp -R ${chibios}/os/hal/include ./portable/ChibiOS-RT_Portable_HAL_Component/com
 cp -R ${chibios}/os/hal/src ./portable/ChibiOS-RT_Portable_HAL_Component/component/lib
 
 # Portable Kernel Test Suite.
+echo "Portable Test Suite"
 cp ${chibios}/test/*.h ./portable/ChibiOS-RT_Portable_Kernel_Test_Suite_Component/component/lib/include/
 cp ${chibios}/test/*.c ./portable/ChibiOS-RT_Portable_Kernel_Test_Suite_Component/component/lib/src/
 
 # Portable Output Formatter.
+echo "Portable Output Formatter"
 cp ${chibios}/os/various/chprintf.h ./portable/ChibiOS-RT_Portable_Output_Formatter_Component/component/lib/include/
 cp ${chibios}/os/various/chprintf.c ./portable/ChibiOS-RT_Portable_Output_Formatter_Component/component/lib/src/
 
 # Portable Shell.
+echo "Portable Shell"
 cp ${chibios}/os/various/shell.h ./portable/ChibiOS-RT_Portable_Mini_Shell_Component/component/lib/include/
 cp ${chibios}/os/various/shell.c ./portable/ChibiOS-RT_Portable_Mini_Shell_Component/component/lib/src/
 
@@ -100,13 +105,18 @@ cp ${chibios}/os/ports/GCC/PPC/SPC56ELxx/vectors.s      ./SPC56ELxx/ChibiOS-RT_S
 # STM32F4xx Components.                                                      #
 #============================================================================#
 
+# STM32F4xx Platform.
+echo "STM32F4xx Platform"
+cp ${chibios}/os/ports/GCC/ARMCMx/rules.mk              ./STM32F4xx/ChibiOS-RT_STM32F4xx_Platform_Component/component/lib/rsc/
+cp ${chibios}/os/ports/GCC/ARMCMx/crt0.c                ./STM32F4xx/ChibiOS-RT_STM32F4xx_Platform_Component/component/lib/src/
+
 # STM32F4xx Kernel Port.
 echo "STM32F4xx Kernel Port"
 cp ${chibios}/os/ports/common/ARMCMx/nvic.h             ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/include/
 cp ${chibios}/os/ports/common/ARMCMx/nvic.c             ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/src/
 cp ${chibios}/os/ports/GCC/ARMCMx/chcore.h              ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/include/
 cp ${chibios}/os/ports/GCC/ARMCMx/chcore_v7m.h          ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/include/
-cp ${chibios}/os/ports/GCC/ARMCMx/chtypes.h.h           ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/include/
+cp ${chibios}/os/ports/GCC/ARMCMx/chtypes.h             ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/include/
 cp ${chibios}/os/ports/GCC/ARMCMx/STM32F4xx/*.h         ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/include/
 cp ${chibios}/os/ports/GCC/ARMCMx/chcore.c              ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/src/
 cp ${chibios}/os/ports/GCC/ARMCMx/chcore_v7m.c          ./STM32F4xx/ChibiOS-RT_STM32F4xx_Kernel_Port_Component/component/lib/src/
@@ -114,5 +124,19 @@ cp ${chibios}/os/ports/GCC/ARMCMx/STM32F4xx/*.c         ./STM32F4xx/ChibiOS-RT_S
 
 # STM32F4xx HAL Platform.
 echo "STM32F4xx HAL Platform"
+cp ${chibios}/os/hal/platforms/STM32/*.h                ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
+cp ${chibios}/os/hal/platforms/STM32/*.c                ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
+cp ${chibios}/os/hal/platforms/STM32/GPIOv2/*.h         ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
+cp ${chibios}/os/hal/platforms/STM32/GPIOv2/*.c         ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
+cp ${chibios}/os/hal/platforms/STM32/I2Cv1/*.h          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
+cp ${chibios}/os/hal/platforms/STM32/I2Cv1/*.c          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
+cp ${chibios}/os/hal/platforms/STM32/OTGv1/*.h          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
+cp ${chibios}/os/hal/platforms/STM32/OTGv1/*.c          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
+cp ${chibios}/os/hal/platforms/STM32/RTCv2/*.h          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
+cp ${chibios}/os/hal/platforms/STM32/RTCv2/*.c          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
+cp ${chibios}/os/hal/platforms/STM32/SPIv1/*.h          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
+cp ${chibios}/os/hal/platforms/STM32/SPIv1/*.c          ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
+cp ${chibios}/os/hal/platforms/STM32/USARTv1/*.h        ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
+cp ${chibios}/os/hal/platforms/STM32/USARTv1/*.c        ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
 cp ${chibios}/os/hal/platforms/STM32F4xx/*.h            ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/include/
 cp ${chibios}/os/hal/platforms/STM32F4xx/*.c            ./STM32F4xx/ChibiOS-RT_STM32F4xx_HAL_Platform_Component/component/lib/src/
