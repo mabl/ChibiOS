361bf192f77039ffcf57ad95e99a86c2f27911a2
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-07-21 08:41:58 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/spc5studio_nil_components_trunk@7045 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ copysrc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+chibios=../../chibios
+#do_templates=yes
+
+#============================================================================#
+# Portable Components.                                                       #
+#============================================================================#
+
+# Portable Kernel.
+echo "Portable Kernel"
+if [ ! -d $do_templates ]
+then
+  cp ${chibios}/os/nil/templates/nilconf.h                  ./portable/NIL_Portable_Kernel_Component/component/cfg/nilconf.h.ftl
+fi
+cp -R ${chibios}/os/nil/include                             ./portable/NIL_Portable_Kernel_Component/component/lib
+cp -R ${chibios}/os/nil/src                                 ./portable/NIL_Portable_Kernel_Component/component/lib
+
+#============================================================================#
+# SPC560Dxx Components.                                                      #
+#============================================================================#
+
+# SPC560Dxx Kernel Port.
+echo "SPC560Dxx Kernel Port"
+cp ${chibios}/os/nil/ports/e200/*.h                         ./SPC560Dxx/NIL_SPC560Dxx_Kernel_Port_Component/component/lib/include/
+cp ${chibios}/os/nil/ports/e200/*.c                         ./SPC560Dxx/NIL_SPC560Dxx_Kernel_Port_Component/component/lib/src/
+cp ${chibios}/os/nil/ports/e200/compilers/GCC/*.h           ./SPC560Dxx/NIL_SPC560Dxx_Kernel_Port_Component/component/lib/include/
+cp ${chibios}/os/nil/ports/e200/compilers/GCC/ivor.s        ./SPC560Dxx/NIL_SPC560Dxx_Kernel_Port_Component/component/lib/src/
+cp ${chibios}/os/common/ports/e200/compilers/GCC/*.h        ./SPC560Dxx/NIL_SPC560Dxx_Kernel_Port_Component/component/lib/include/
+cp ${chibios}/os/common/ports/e200/compilers/GCC/vectors.s  ./SPC560Dxx/NIL_SPC560Dxx_Kernel_Port_Component/component/lib/src/
+cp ${chibios}/os/common/ports/e200/devices/SPC560Dxx/*.h    ./SPC560Dxx/NIL_SPC560Dxx_Kernel_Port_Component/component/lib/include/
+
+echo ""
+echo "Done!"
