0fb9f2f437cddac053f9726d105b51bab43f7b92
wan35fr <wan35fr@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-11-06 16:49:09 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/spc5studio_nil_components_trunk@7486 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ demos/NIL RTOS SPC560Dxx Test Application for Discovery/main.c
@@ -0,0 +1,37 @@
+/*
+    SPC5 HAL - Copyright (C) 2013 STMicroelectronics
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/* Inclusion of the main header files of all the imported components in the
+   order specified in the application wizard. The file is generated
+   automatically.*/
+#include "components.h"
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /* Initialization of all the imported components in the order specified in
+     the application wizard. The function is generated automatically.*/
+  componentsInit();
+
+  /* This is now the idle thread loop, you may perform here a low priority
+     task but you must never try to sleep or wait in this loop. Note that
+     this tasks runs at the lowest priority level so any instruction added
+     here will be executed after all other tasks have been started.*/
+  while (1) {
+  }
+}
