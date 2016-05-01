a9b7c42d8adf913f1bce7eedfd181ede8908f720
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2016-05-01 09:14:24 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/chibios_components_trunk@9395 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- copysrc.sh
+++ copysrc.sh
@@ -18,17 +18,17 @@ cp -f ${chibios}/os/license/*.h                                             ./po
 
 # ChibiOS/RT Kernel for ARMv7-M.
 echo "ChibiOS/RT Kernel for ARMv7-M files"
-cp -f ${chibios}/os/rt/src/*.c                                              ./rt/chibios_rt_armv7m_component/component/lib/src/
-cp -f ${chibios}/os/rt/include/*.h                                          ./rt/chibios_rt_armv7m_component/component/lib/include/
-cp -f ${chibios}/os/common/oslib/src/*.c                                    ./rt/chibios_rt_armv7m_component/component/lib/src/
-cp -f ${chibios}/os/common/oslib/include/*.h                                ./rt/chibios_rt_armv7m_component/component/lib/include/
-cp -f ${chibios}/os/common/ports/ARMCMx/chcore.c                            ./rt/chibios_rt_armv7m_component/component/lib/src/
-cp -f ${chibios}/os/common/ports/ARMCMx/chcore_v7m.c                        ./rt/chibios_rt_armv7m_component/component/lib/src/
-cp -f ${chibios}/os/common/ports/ARMCMx/chcore.h                            ./rt/chibios_rt_armv7m_component/component/lib/include/
-cp -f ${chibios}/os/common/ports/ARMCMx/chcore_v7m.h                        ./rt/chibios_rt_armv7m_component/component/lib/include/
-cp -f ${chibios}/os/common/ports/ARMCMx/mpu.h                               ./rt/chibios_rt_armv7m_component/component/lib/include/
-cp -f ${chibios}/os/common/ports/ARMCMx/compilers/GCC/chcoreasm_v7m.S       ./rt/chibios_rt_armv7m_component/component/lib/src/
-cp -f ${chibios}/os/common/ports/ARMCMx/compilers/GCC/*.h                   ./rt/chibios_rt_armv7m_component/component/lib/include/
+cp -f ${chibios}/os/rt/src/*.c                                          ./rt/chibios_rt_armv7m_component/component/lib/src/
+cp -f ${chibios}/os/rt/include/*.h                                      ./rt/chibios_rt_armv7m_component/component/lib/include/
+cp -f ${chibios}/os/common/oslib/src/*.c                                ./rt/chibios_rt_armv7m_component/component/lib/src/
+cp -f ${chibios}/os/common/oslib/include/*.h                            ./rt/chibios_rt_armv7m_component/component/lib/include/
+cp -f ${chibios}/os/common/ports/ARMCMx/chcore.c                        ./rt/chibios_rt_armv7m_component/component/lib/src/
+cp -f ${chibios}/os/common/ports/ARMCMx/chcore_v7m.c                    ./rt/chibios_rt_armv7m_component/component/lib/src/
+cp -f ${chibios}/os/common/ports/ARMCMx/chcore.h                        ./rt/chibios_rt_armv7m_component/component/lib/include/
+cp -f ${chibios}/os/common/ports/ARMCMx/chcore_v7m.h                    ./rt/chibios_rt_armv7m_component/component/lib/include/
+cp -f ${chibios}/os/common/ports/ARMCMx/mpu.h                           ./rt/chibios_rt_armv7m_component/component/lib/include/
+cp -f ${chibios}/os/common/ports/ARMCMx/compilers/GCC/chcoreasm_v7m.S   ./rt/chibios_rt_armv7m_component/component/lib/src/
+cp -f ${chibios}/os/common/ports/ARMCMx/compilers/GCC/*.h               ./rt/chibios_rt_armv7m_component/component/lib/include/
 
 #============================================================================#
 # STM32F4xx Components.                                                      #
@@ -36,15 +36,19 @@ cp -f ${chibios}/os/common/ports/ARMCMx/compilers/GCC/*.h                   ./rt
 
 # STM32F4xx Startup.
 echo "STM32F4xx Startup files"
-cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/rules.mk            ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/rsc/
-cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/ld/rules*.ld        ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/rsc/ld
-cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/*.c                 ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/src/
-cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.S          ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/src/
-cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/*.h                 ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/
-cp -f ${chibios}/os/common/startup/ARMCMx/devices/STM32F4xx/*.h             ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/
-cp -f ${chibios}/os/common/ext/CMSIS/ST/STM32F4xx/*.h                       ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/device
-cp -f ${chibios}/os/common/ext/CMSIS/include/*.h                            ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/cmsis
+cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/rules.mk        ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/rsc/
+cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/ld/rules*.ld    ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/rsc/ld
+cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/*.c             ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/src/
+cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.S      ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/src/
+cp -f ${chibios}/os/common/startup/ARMCMx/compilers/GCC/*.h             ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/
+cp -f ${chibios}/os/common/startup/ARMCMx/devices/STM32F4xx/*.h         ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/
+cp -f ${chibios}/os/common/ext/CMSIS/ST/STM32F4xx/*.h                   ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/device
+cp -f ${chibios}/os/common/ext/CMSIS/include/*.h                        ./platforms/STM32F4xx/chibios_stm32f4xx_startup_component/component/lib/include/cmsis
 
+# STM32F4xx HAL.
+echo "STM32F4xx HAL files"
+cp -f ${chibios}/os/hal/src/*.c                                         ./platforms/STM32F4xx/chibios_stm32f4xx_hal_component/component/lib/src/
+cp -f ${chibios}/os/hal/include/*.h                                     ./platforms/STM32F4xx/chibios_stm32f4xx_hal_component/component/lib/include/
 
 echo ""
 echo "Done!"
